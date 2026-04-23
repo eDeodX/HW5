0c2152fab352cffbdde00c9d3960bcbbecffbd96
eDeodX <hanromik124@gmail.com>
2026-04-23 21:29:49 +0300

    fix GMOCK in Account + CMakeLists.txt

--- tests/test_account.cpp
+++ tests/test_account.cpp
@@ -3,6 +3,16 @@
 #include "Account.h"
 #include <stdexcept>
 
+class MockAccount : public Account {
+public:
+	MockAccount(int id, int balance) : Account(id, balance) {}
+
+	MOCK_METHOD(int, GetBalance, (), (const, override));
+	MOCK_METHOD(void, ChangeBalance, (int diff), (override));
+	MOCK_METHOD(void, Lock, (), (override));
+	MOCK_METHOD(void, Unlock, (), (override));
+};
+
 TEST(AccountTest, ConstructorAccount) {
 	int id = 1000, balance = 10;
 	Account acc(id, balance);
@@ -51,4 +61,28 @@ TEST(AccountTest, ChangeBalanceUnlock) {
 	Account acc(1000, 10);
 	EXPECT_THROW(acc.ChangeBalance(5), std::runtime_error);
 	EXPECT_EQ(acc.GetBalance(), 10);
+}
+
+TEST(MockAccountTest, TestLock) {
+	MockAccount acc(1, 100);
+	EXPECT_CALL(acc, Lock());
+	acc.Lock();
+}
+
+TEST(MockAccountTest, TestUnlock) {
+	MockAccount acc(1, 100);
+	EXPECT_CALL(acc, Unlock());
+	acc.Unlock();
+}
+
+TEST(MockAccountTest, TestGetBalance) {
+	MockAccount acc(1, 100);
+	EXPECT_CALL(acc, GetBalance()).WillOnce(testing::Return(100));
+	EXPECT_EQ(acc.GetBalance(), 100);
+}
+
+TEST(MockAccountTest, TestChangeBalance) {
+	MockAccount acc(1, 100);
+	EXPECT_CALL(acc, ChangeBalance(50));
+	acc.ChangeBalance(50);
 }
